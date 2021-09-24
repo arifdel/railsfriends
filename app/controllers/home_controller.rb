67ecc4ab4313a179274f8d07273f2a60f95b0fe2
arifdel <arif.del95@gmail.com>
2021-09-24 09:32:50 +0530

    updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
   end
   def about
-  	@about_me = "My name is Arif Siddiqui..."
+  	@about_me = "We maintain your friends list"
   end
 end
